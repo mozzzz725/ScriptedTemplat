b8271acf240a56f97cbb12870d416c18fab993f8
ari <mozzzz725@gmail.com>
2025-07-19 20:45:53 +0330

    'init02'

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='user_profile')
+
+    def __str__(self):
+        return self.bio

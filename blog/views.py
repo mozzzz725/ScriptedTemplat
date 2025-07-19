d68ebc0196205e15569f215914d10a3b4df22ef3
Arsam <mozzzz725@gmail.com>
2025-07-19 18:29:43 +0330

    'init01'

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+
+def post(request):
+    return render(request, 'blog/post.html', {})
+
+
+def posts(request):
+    return render(request, 'blog/posts.html', {})
+
